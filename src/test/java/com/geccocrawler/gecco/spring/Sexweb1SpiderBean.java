2531fc61a5174579544894ff5e99a90eb4a1cfde
nobody <anythingbyme@protonmail.com>
2017-12-16 12:48:29 +0800

    可以抓取图片地址了

--- src/test/java/com/geccocrawler/gecco/spring/Sexweb1SpiderBean.java
+++ src/test/java/com/geccocrawler/gecco/spring/Sexweb1SpiderBean.java
@@ -1,14 +1,21 @@
 package com.geccocrawler.gecco.spring;
 
 import com.geccocrawler.gecco.annotation.*;
+import com.geccocrawler.gecco.pipeline.Pipeline;
 import com.geccocrawler.gecco.request.HttpRequest;
 import com.geccocrawler.gecco.spider.HtmlBean;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /***
- * 处理	//http://w3.afulyu.rocks/pw/htm_data/14/1712/916063.html 的spiderBean
+ * 处理	http://w3.afulyu.rocks/pw/htm_data/14/1712/916063.html 的spiderBean
  */
-@Gecco(matchUrl="http://w3.afulyu.rocks/pw/htm_data/{date}/{yearmonth}/{theone}.html", pipelines="consolePipeline")
-public class Sexweb1SpiderBean implements HtmlBean {
+//@Gecco(matchUrl="http://w3.afulyu.rocks/pw/htm_data/{date}/{yearmonth}/{theone}.html", pipelines="consolePipeline")
+//@PipelineName("sexweb1SpiderBean")
+@Service("sexweb1SpiderBean")
+@Gecco(matchUrl="http://w3.afulyu.rocks/pw/htm_data/{date}/{yearmonth}/{theone}.html", pipelines="sexweb1SpiderBean")
+public class Sexweb1SpiderBean implements HtmlBean, Pipeline<Sexweb1SpiderBean> {
 
 
 	private static final long serialVersionUID = -7127412585200687225L;
@@ -28,6 +35,13 @@ public class Sexweb1SpiderBean implements HtmlBean {
 	@Text
 	@HtmlField(cssPath="#subject_tpc")
 	private String title;
+
+
+	//抓取图片的src地址.
+	// 图片的src的List.
+	@Image
+	@HtmlField(cssPath = "#read_tpc > img")
+	public List<String> pics;
 
 
 	public HttpRequest getRequest() {
@@ -68,5 +82,22 @@ public class Sexweb1SpiderBean implements HtmlBean {
 
 	public void setTitle(String title) {
 		this.title = title;
+	}
+
+
+	public List<String> getPics() {
+		return pics;
+	}
+
+	public void setPics(List<String> pics) {
+		this.pics = pics;
+	}
+
+	@Override
+	public void process(Sexweb1SpiderBean sexweb1SpiderBean) {
+		List<String> pics = sexweb1SpiderBean.getPics();
+		for (int i = 0; i < pics.size(); i++) {
+			System.out.println(pics.get(i));
+		}
 	}
 }
